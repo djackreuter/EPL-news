d57f4b949d0a4d363463237bd9f7f9af330c45d9
Jack Reuter <djreuter45@gmail.com>
2018-04-10 12:37:06 -0600

    added urls to requests

--- main/main.py
+++ main/main.py
@@ -1,9 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
 
-liverpool = 'http://www.liverpoolfc.com/news/first-team'
-man_city  = 'https://www.mancity.com/news/first-team'
-man_utd   = 'http://www.manutd.com/en/News-And-Features/Club-News.aspx'
-tottenham = 'http://www.tottenhamhotspur.com/news/'
-
+liverpool = requests.get('http://www.liverpoolfc.com/news/first-team')
+man_city  = requests.get('https://www.mancity.com/news/first-team')
+man_utd   = requests.get('http://www.manutd.com/en/News-And-Features/Club-News.aspx')
+tottenham = requests.get('http://www.tottenhamhotspur.com/news/')
 
