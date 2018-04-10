e826af0a5d3fd62874c5536dae195fe619bbe9c8
Jack Reuter <djreuter45@gmail.com>
2018-04-10 10:13:20 -0600

    added teams and urls

---
+++ main/main.py
@@ -0,0 +1,9 @@
+import requests
+from bs4 import BeautifulSoup
+
+liverpool = 'http://www.liverpoolfc.com/news/first-team'
+man_city  = 'https://www.mancity.com/news/first-team'
+man_utd   = 'http://www.manutd.com/en/News-And-Features/Club-News.aspx'
+tottenham = 'http://www.tottenhamhotspur.com/news/'
+
+
