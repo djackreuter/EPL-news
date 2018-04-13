99d2ac301ecdde6415c37da1151905cb26709a49
Jack Reuter <djreuter45@gmail.com>
2018-04-13 14:14:55 -0600

    started scraping article descriptions for liverpool and manchester city

--- main/main.py
+++ main/main.py
@@ -6,3 +6,12 @@ man_city  = requests.get('https://www.mancity.com/news/first-team')
 man_utd   = requests.get('http://www.manutd.com/en/News-And-Features/Club-News.aspx')
 tottenham = requests.get('http://www.tottenhamhotspur.com/news/')
 
+liverpool_news = BeautifulSoup(liverpool.text, 'html.parser')
+liverpool_news_summaries = liverpool_news.find_all(class_=['post-synopsis', 'synopsis'])
+print(liverpool_news_summaries)
+
+man_city_news = BeautifulSoup(man_city.text, 'html.parser')
+man_city_articles = man_city_news.find_all(class_='article--preview-body')
+for h in man_city_articles:
+    print(h.find_next('h2'))
+    
